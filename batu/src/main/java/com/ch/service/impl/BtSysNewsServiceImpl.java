fcc0069b03cdf05047e30805533a9f288d0c82be
常富文 <1712710548@qq.com>
2019-03-23 17:19:49 +0800

    修改新闻接口

--- batu/src/main/java/com/ch/service/impl/BtSysNewsServiceImpl.java
+++ batu/src/main/java/com/ch/service/impl/BtSysNewsServiceImpl.java
@@ -200,6 +200,12 @@ public class BtSysNewsServiceImpl implements BtSysNewsService {
         result.setData(page);
         return result;
     }
+    public ResponseResult findById(String id){
+        ResponseResult result = new ResponseResult();
+        BtViewNews btViewNews = btViewNewsMapper.selectByPrimaryKey(id);
+        result.setData(btViewNews);
+        return  result;
+    }
 }
 
 
