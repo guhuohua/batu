3ea66894b3aac9b4d0ccf2a79ae5d67fb75ff805
Kyle.chang <1210949654@qq.com>
2019-03-26 11:18:00 +0800

    修改bug

--- batu/src/main/java/com/ch/service/impl/BtSysNewsServiceImpl.java
+++ batu/src/main/java/com/ch/service/impl/BtSysNewsServiceImpl.java
@@ -38,8 +38,6 @@ public class BtSysNewsServiceImpl implements BtSysNewsService {
     @Autowired
     BtViewNewsFanMapper btViewNewsFanMapper;
 
-    @Autowired
-    BtViewMenuMapper btViewMenuMapper;
 
     @Override
     public long countByExample(BtViewNewsExample example) {
@@ -182,20 +180,6 @@ public class BtSysNewsServiceImpl implements BtSysNewsService {
     @Override
     public ResponseResult updateStatus(String id, int status) {
         ResponseResult result = new ResponseResult();
-        BtViewNews btViewNews = btViewNewsMapper.selectByPrimaryKey(id);
-        BtViewMenu btViewMenu = btViewMenuMapper.selectByPrimaryKey(btViewNews.getMenuId());
-        if (btViewMenu.getPage()) {
-            BtViewNewsExample example = new BtViewNewsExample();
-            BtViewNewsExample.Criteria criteria = example.createCriteria();
-            criteria.andMenuIdEqualTo(btViewMenu.getId());
-            List<BtViewNews> btViewNewsList = btViewNewsMapper.selectByExample(example);
-            for (BtViewNews btViewNews1 : btViewNewsList) {
-                String newId = btViewNews1.getId();
-                btViewNewsMapper.updateStatus(newId, 0);
-                btViewNewsEngMapper.updateStatus(newId,0);
-                btViewNewsFanMapper.updateStatus(newId,0);
-            }
-        }
         if (status == 1) {
             BtViewNews viewNews = btViewNewsMapper.findById(id);
             BtViewMenu btViewMenu = btViewMenuMapper.findById(viewNews.getMenuId());
@@ -208,8 +192,10 @@ public class BtSysNewsServiceImpl implements BtSysNewsService {
         } else {
             btViewNewsMapper.updateDate(id, status);
         }
+
         btViewNewsEngMapper.updateStatus(id, status);
         btViewNewsFanMapper.updateStatus(id, status);
+
         return result;
     }
 
